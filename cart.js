ff0b2fd9800db53511792b7368e4d10c935427b0
Taylor Haase <taylor_haase@hotmail.com>
2017-08-30 19:17:27 -0700

    finished shopping cart

--- cart.js
+++ cart.js
@@ -1,10 +1,55 @@
-$(function(){
+/*var price = $(".button").click(parseFloat($(this).siblings("p").text().substring(1)));
+var name = $(".button").click(this).siblings("h2").text();*/
+
+var items = [];
+targetArea = $("#cart");
+const subtotalContainer = $("#subtotal");
+const taxContainer = $("#tax");
+const totalContainer = $("#total");
+
+$(function addToCart(){
     $(".button").click(function(){
-        let item = $(this).siblings().text();
-            targetArea = $("#cart");
-        targetArea.append(item)
-    })
-})
+        targetArea.empty();
+        let price = parseFloat($(this).siblings("p").text().substring(1));
+        let name = $(this).siblings("h2").text()
+        let this_item = {};
+        this_item.name = name;
+        this_item.price = price;
+
+        items.push(this_item);
+
+        items.forEach(function(item){
+            let itemElement = "<p>" + item.name + ": " + item.price + "</p>"
+            targetArea.append(itemElement);
+        })
+    let subtotal = items.reduce(function(accumulator, currentItem){
+        return accumulator + currentItem.price;
+    }, 0);
+
+    let subtotalElement = "<p>" + subtotal + "</p>"
+    subtotalContainer.empty();
+    subtotalContainer.append(subtotalElement);
+
+    let notFixedTax = subtotal * .10;
+    let tax = notFixedTax.toFixed(2);
+
+    let taxElement = "<p>" + tax + "</p>"
+    taxContainer.empty();
+    taxContainer.append(taxElement);
+
+    let notFixedTotal = subtotal + notFixedTax;
+    let total = notFixedTotal.toFixed(2);
+
+    let totalElement = "<p>" + total + "</p>"
+    totalContainer.empty();
+    totalContainer.append(totalElement);
+
+    });
+});
+
+
 $( ".submit-button" ).click(function() {
   alert( "Your Order Has Been Placed!" );
 });
+
+
